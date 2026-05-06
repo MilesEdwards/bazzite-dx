ba1f975382af839fd4d7c2d2b825bda248b42ed1
Miles Edwards <milesdedwards@gmail.com>
2026-05-06 16:49:24 +0100

    chore: remove -deck specific actions

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -43,12 +43,12 @@ systemctl enable input-remapper.service
 systemctl enable uupd.timer
 
 # Remove -deck specific changes to allow for login screens and session selection in settings
-rm -f /etc/sddm.conf.d/steamos.conf
-rm -f /etc/sddm.conf.d/virtualkbd.conf
-rm -f /etc/sddm.conf.d/zz-steamos-autologin.conf
-rm -f /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz
-systemctl disable bazzite-autologin.service
-dnf5 remove -y steamos-manager
+# rm -f /etc/sddm.conf.d/steamos.conf
+# rm -f /etc/sddm.conf.d/virtualkbd.conf
+# rm -f /etc/sddm.conf.d/zz-steamos-autologin.conf
+# rm -f /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz
+# systemctl disable bazzite-autologin.service
+# dnf5 remove -y steamos-manager
 
 # if [[ "$IMAGE_NAME" == *gnome* ]]; then
 #     # Remove SDDM and re-enable GDM on GNOME builds.
